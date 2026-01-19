723ef3603358439a861a3d340e70f8bd204af546
Jashandeep <jashan.07deep2@gmail.com>
2026-01-19 10:29:20 +0530

    First git

---
+++ script.js
@@ -0,0 +1,21 @@
+const button=document.querySelector('button')
+// const button2=document.querySelector('#bttt')
+// button.classList.add('btn')
+
+// function message(){
+//     alert("Peak")
+
+// }
+// button.addEventListener('click',message)
+
+
+
+// button2.addEventListener('click',function(){
+//     button.removeEventListener('click',message)
+// })
+// button.addEventListener('keydown',function(event){
+//     console.log(event.key)
+// })
+button.addEventListener('keyup',function(event){
+    console.log(event.key)
+})
