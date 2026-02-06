8026a1212db8ab053e5c0af3b2f42168adbf6aea
Jashandeep <jashan.07deep2@gmail.com>
2026-02-06 15:45:19 +0530

    6 Feb

--- script.js
+++ script.js
@@ -99,26 +99,105 @@ const button=document.querySelector('button')
 
 // To face callback hell
 
-function finishhomework(callback) {
-    console.log("Starting Homework")
-    setTimeout(()=>{
-        console.log("Homework done");
-        callback();
-    },2000)
-}
-function eatdinner(callback){
-    console.log("Starting Dinner")
-    setTimeout(()=>{
-        console.log("Dinner done");
-        callback();
-    },1500);
-}
-function goToPlayground(){
-    console.log("Going to the playground");
+// function finishhomework(callback) {
+//     console.log("Starting Homework")
+//     setTimeout(()=>{
+//         console.log("Homework done");
+//         callback();
+//     },2000)
+// }
+// function eatdinner(callback){
+//     console.log("Starting Dinner")
+//     setTimeout(()=>{
+//         console.log("Dinner done");
+//         callback();
+//     },1500);
+// }
+// function goToPlayground(){
+//     console.log("Going to the playground");
+// }
+
+// finishhomework(()=>{
+//     eatdinner(()=>{
+//         goToPlayground();
+//     })
+// })
+
+// Promie statement
+
+// const p=new Promise(function(resolve,reject){
+//     setTimeout(()=>{
+//         let done=true;
+//         if(done){
+//             resolve({name:"Alex",age:34})
+//         }else{
+//             reject("Network issue")
+//         }
+//     },5000)
+// })
+// p.then((data)=>{
+//     console.log("Resolved",data)
+// }).catch((err)=>{
+//     console.log("Rejected",err)
+// }).finally(()=>{
+//     console.log("Finally block")
+// })
+
+function doHomework(){
+    const p=new Promise(function(resolve,reject){
+        setTimeout(()=>{
+            let done=true;
+            if(done){
+                console.log("Homework is done")
+                resolve("Homework complete")
+            }else{
+                reject("Homework is not done")
+            }
+        },5000)
+    })
+    return p
 }
 
-finishhomework(()=>{
-    eatdinner(()=>{
-        goToPlayground();
+function eatDinner(){
+    const p=new Promise(function(resolve,reject){
+        setTimeout(()=>{
+            let done=true;
+            if(done){
+                console.log("Eating Dinner")
+                resolve("Dinner complete")
+            }else{
+                reject("Dinner is not done")
+            }
+        },5000)
     })
+    return p
+}
+
+function goToPlayground(){
+    const p=new Promise(function(resolve,reject){
+        setTimeout(()=>{
+            let done=true;
+            if(done){
+                console.log("Went to playground")
+                resolve("Playground Time")
+            }else{
+                reject("Go to sleep")
+            }
+        },5000)
+    })
+    return p
+}
+
+doHomework().then((data)=>{
+    console.log(data)
+    return eatDinner()
+}).then((data)=>{
+    console.log(data)
+    return goToPlayground()
+}).then((data)=>{
+    console.log(data)
+}).catch((err)=>{
+    console.log(err)
+}).finally(()=>{
+    console.log("Go to Sleep")
 })
